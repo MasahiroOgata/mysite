68f2aaaca648f7568402caf1f9aa0f6bdd3a1e66
MasahiroOgata <ogata0801@gmail.com>
2025-05-01 15:34:13 +0900

    viewの変更

--- bbs/views.py
+++ bbs/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'bbs/index.html')
+    context = {
+        'message' : 'Welcome my BBS',
+    }
+    return render(request, 'bbs/index.html', context)
